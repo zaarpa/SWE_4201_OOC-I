58d7b8d4ebe7b602d1350ab645fca93017f65d5d
Zaara Zabeen Arpa <95168733+zaarpa@users.noreply.github.com>
2021-12-17 11:02:56 +0600

    Add files via upload

---
+++ Pharmacy_Dependencies/ACCOUNT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Dependencies
+{
+    public class ACCOUNT 
+    {
+        public double accountBalance = 1000;
+        public void addBalance(double n)
+        {
+            accountBalance += n;
+        }
+        public void subBalance(double n)
+        {
+            accountBalance -= n;
+        }
+        public double getBalance()
+        {
+            return accountBalance; 
+        }
+    }
+} 
---
+++ Pharmacy_Dependencies/MEDICINE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Dependencies
+{
+    public class MEDICINE
+    {
+        public string medicineName;
+        public double sellPrice, buyPrice;
+        public int medicineAmount;
+
+        public string getMedicineInfo()
+        {
+            string medName = Convert.ToString(medicineAmount);
+            return medicineName + "\t\t\t" + medName;
+        } 
+    }
+} 
